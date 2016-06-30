0141d7e0426e4191029fb86918f4ea530abedb09
Lawrence Chen <lawrenceochen@gmail.com>
2016-06-30 15:23:31 -0700

    serviceworker attempts to load from network before defaulting to cache

--- sw.js
+++ sw.js
@@ -1,39 +1,47 @@
 var CACHENAME = "testcache";
-var cacheResources = [
-	"index.html",
-	"style.css",
-	"app.js",
-	"db.js",
-	"favicon.ico"
-]
+var cacheResources = ["index.html", "/", "style.css", "app.js", "db.js", "sw.js"];
 
-self.addEventListener("install", function(event) {
-	event.waitUntil(
-		caches.open(CACHENAME).then(function(cache) {
-			console.log("[install] saving resources to cache");
-			return cache.addAll(cacheResources);
-		}).then(function() {
-			console.log("[install] cached all resources");
-			return self.skipWaiting();
-		})
-	);
+self.addEventListener("install", function (event) {
+	console.log("[install] saving resources to cache");
+	event.waitUntil(caches.open(CACHENAME).then(function (cache) {
+		return cache.addAll(cacheResources);
+	}).then(function () {
+		console.log("[install] cached all resources");
+		return self.skipWaiting();
+	}));
 });
 
-self.addEventListener("fetch", function(event){
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			if (response) {
-				console.log("[fetch] responding using serviceworker cache", event.request.url);
+// override fetch()'s default behavior to look in the cache first
+var nocacheHeaders = new Headers();
+nocacheHeaders.append("pragma", "no-cache");
+nocacheHeaders.append("cache-control", "no-cache");
+self.addEventListener("fetch", function (event) {
+	var request = event.request;
+	if (request.method === "GET") {
+		event.respondWith(
+			fetch(request.url, {
+				method: request.method,
+				headers: nocacheHeaders,
+				mode: "same-origin",
+				credentials: request.credentials,
+				redirect: "manual"
+			}).then(function (response) {
+				console.log("pulling", response, "from network");
 				return response;
-			}
-			
-			console.log("[fetch] responding using network", event.request.url);
-			return fetch(event.request);
-		})
-	);
+			}).catch(function (error) {
+				return caches.open(CACHENAME)
+					.then(function (cache) {
+						return cache.match(request);
+					}).then(function (response) {
+						console.log("pulling", response, "from cache");
+						return response;
+					});
+			})
+		);
+	}
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
 	console.log("[activate] claiming serviceworker");
 	event.waitUntil(self.clients.claim());
 });
