eb95060e88c2ab4e64aea429134d5d13e73b67ce
Lawrence Chen <lawrenceochen@gmail.com>
2016-08-01 11:09:48 -0700

    modified bootstrap js to work with apex page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     if(!navigator.serviceWorker) return;
     
-    navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function(reg) {
+    navigator.serviceWorker.register('../sw.js', {scope: './'}).then(function(reg) {
         if(reg.waiting) {
             console.log('[register-waiting] Update is ready and waiting!!');
             return;
@@ -34,42 +34,5 @@ window.onload = function() {
         });
     }
     
-    // ask user permission to send notifications, then add the reg button
-    new Promise(function(resolve, reject) {
-        Notification.requestPermission(function(result) {
-            if (result !== 'granted') {
-                return reject(Error("Denied notification permission"));
-            }
-
-            resolve();
-        });
-    }).then(function() {
-        var notiDiv = document.getElementById('notiDiv');
-        var input = document.createElement('input');
-        
-        input.setAttribute('id', 'syncBtn');
-        input.setAttribute('type', 'button');
-        input.setAttribute('value', 'Register background sync!');
-        notiDiv.appendChild(input);
-
-        document.getElementById('syncBtn').addEventListener('click', function(event) {
-            event.preventDefault();
-            // Request a sync
-            navigator.serviceWorker.ready.then(function(reg) {
-                var item = 'outbox-' + Date.now();
-                reg.sync.register(item).then(function() {
-                    // registration succeeded
-                    var log = document.createElement('p');
-                    log.textContent = item + ' registered!';
-                    notiDiv.appendChild(log);
-                }, function() {
-                    // registration failed
-                    var log = document.createElement('p');
-                    log.textContent = 'Registration failed';
-                    notiDiv.appendChild(log);
-                });
-            });
-        });
-    });
 }
 
