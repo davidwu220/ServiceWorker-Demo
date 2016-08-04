aa1d9aade8b98dd4a457d1dcd86a505d2b5b66df
Wu <dwu@princesscruises.com>
2016-08-04 16:07:49 -0700

    added the db file

---
+++ js/db.js
@@ -0,0 +1,74 @@
+var _VERSION = 1;
+var _DBNAME = 'drydockFields';
+var _STORENAME = 'drydockData';
+
+var apexDB = (function() {
+    var aDB = {};
+    var datastore = null;
+    
+    
+    // open a connection to hte datastore
+    aDB.open = function(callback) {
+
+        var request = indxedDB.open(_DBNAME, _VERSION);
+        
+        request.onupgradeneeded = function(event) {
+            var db = event.target.result;
+            
+            event.target.transaction.onerror = aDB.onerror;
+            
+            // Delete old datastores
+            if(db.objectStoreNames.contains(_STORENAME)) {
+                db.deleteObjectStore(_STORENAME);
+            }
+            
+            // Create a new datastore
+            var store = db.createObjectStore(_STORENAME, {
+                keyPath: 'id'
+            });
+        };
+        
+        // Handle successful datastore access
+        request.onsuccess = function(event) {
+            datastore = event.target.result;
+            callback();
+        };
+        
+        // Handle errors when opening the datastore
+        request.onerror = aDB.onerror;
+    }
+    
+    aDB.fetchFields = function(callback) {
+        var db = datastore;
+        var transaction = db.transaction([_STORENAME], 'readwrite');
+        var objStore = transaction.objectStore(_STORENAME);
+        
+        var keyRange = IDBKeyRange.lowerBound(0);
+        var cursorRequest = objStore.openCursor(keyRange);
+        
+        var inputs = [];
+        // Triggered for each item successfully returned from the database
+        cursorRequest.onsuccess = function(event) {
+            var result = event.target.result;
+            
+            // Make sure the result has a input item
+            if(!!result == false) return;
+            
+            inputs.push(result.value);
+            result.continue();
+        };
+        
+        // Return the inputs list when transaction is complete
+        transaction.oncomplete = function(event) {
+            callback(inputs);
+        };
+        
+        cursorRequest.onerror = aDB.onerror;
+    };
+    
+    aDB.saveFieldData = function(id, value, callback) {
+        
+    };
+    
+    return aDB;
+}());
